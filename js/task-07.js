279ed82dc032f57edf900c36c0d9d5a992056b93
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-08-09 19:42:19 +0200

    deleted unnecessary console.log from task 07

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,5 @@ const control = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
 control.addEventListener("input", () => {
-  console.log(control.value);
   text.style.fontSize = `${control.value}px`;
 });
